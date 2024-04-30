64f22485278e7210aec74de99e92f3c805f78a33
Dov Benyomin Sohacheski <b@kloud.email>
2024-04-30 09:13:39 +0300

    📦 Release v0.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "ws-cli",
 	Short:   "⚡ CLI companion to charge the workspace batteries",
-	Version: "v0.0.1",
+	Version: "v0.0.3",
 	Aliases: []string{"ws"},
 }
 
