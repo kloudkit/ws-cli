c76f9107903111e546128ecd07e8733fed0ea552
Dov Benyomin Sohacheski <b@kloud.email>
2024-09-09 14:22:46 +0300

    🔥 Remove copy function, use built-in

--- cmd/clipboard/clipboard.go
+++ cmd/clipboard/clipboard.go
@@ -17,37 +17,6 @@ var ClipboardCmd = &cobra.Command{
 	Short:   "Interact with the native clipboard",
 }
 
-var copyCmd = &cobra.Command{
-	Use:     "copy",
-	Aliases: []string{"cp"},
-	Args:    cobra.NoArgs,
-	Short:   "Copy piped content to the clipboard",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		piped, err := io.ReadAll(os.Stdin)
-
-		if err != nil {
-			return fmt.Errorf("failed to read from stdin: %v", err)
-		}
-
-		client := net.GetIPCClient()
-
-		req, err := http.NewRequest("POST", "http://localhost/clipboard", bytes.NewReader(piped))
-		if err != nil {
-			return fmt.Errorf("error sending to workspace socket: %v", err)
-		}
-
-		req.Header.Set("Content-Type", "text/plain")
-
-		resp, err := client.Do(req)
-		if err != nil {
-			return fmt.Errorf("error making request: %v", err)
-		}
-		resp.Body.Close()
-
-		return nil
-	},
-}
-
 var pasteCmd = &cobra.Command{
 	Use:   "paste",
 	Short: "Paste clipboard content to the terminal",
@@ -70,5 +39,5 @@ var pasteCmd = &cobra.Command{
 }
 
 func init() {
-	ClipboardCmd.AddCommand(copyCmd, pasteCmd)
+	ClipboardCmd.AddCommand(pasteCmd)
 }
