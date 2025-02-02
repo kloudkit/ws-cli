0f572318e6d72d343c036a8a8c3ff05c0bbb202c
Dov Benyomin Sohacheski <b@kloud.email>
2025-02-02 08:10:00 +0200

    🩹 Patch manifest versioning

--- cmd/info/info.go
+++ cmd/info/info.go
@@ -4,29 +4,50 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+  "strings"
 
 	"github.com/spf13/cobra"
 )
 
-func readJson(key string) string {
-	var pkg map[string]interface{}
+func readJsonFile() map[string]interface{} {
+  var content map[string]interface{}
 
 	data, _ := os.ReadFile("/.manifest")
 
-	_ = json.Unmarshal(data, &pkg)
+	_ = json.Unmarshal(data, &content)
 
-	value, _ := pkg[key].(string)
+  return content
+}
 
-	return value
+func readJson(content map[string]interface{}, key string) string {
+  keys := strings.Split(key, ".")
+	var value interface{} = content
+
+	for _, k := range keys {
+		m, ok := value.(map[string]interface{})
+		if !ok {
+			return ""
+		}
+
+		value = m[k]
+	}
+
+	if str, ok := value.(string); ok {
+		return str
+	}
+
+	return fmt.Sprintf("%v", value)
 }
 
 var InfoCmd = &cobra.Command{
 	Use:   "info",
 	Short: "Display workspace information",
 	Run: func(cmd *cobra.Command, args []string) {
+    var content = readJsonFile()
+
 		fmt.Fprintln(cmd.OutOrStdout(), "Versions")
-		fmt.Fprintln(cmd.OutOrStdout(), "  workspace\t", readJson("version"))
+		fmt.Fprintln(cmd.OutOrStdout(), "  workspace\t", readJson(content, "version"))
 		fmt.Fprintln(cmd.OutOrStdout(), "  ws-cli\t", Version)
-		fmt.Fprintln(cmd.OutOrStdout(), "  VSCode\t", readJson("vscode"))
+		fmt.Fprintln(cmd.OutOrStdout(), "  VSCode\t", readJson(content, "vscode.version"))
 	},
 }
